08c2ff8b544c53f1473b168518a3577d0e250213
Jackson Sanchez <angeljackson0933@gmail.com>
2024-06-02 22:35:22 -0500

    Add prettier, autotag, on save prettier

--- lua/plugins/colorschema.lua
+++ lua/plugins/colorschema.lua
@@ -1,8 +1,9 @@
 return {
-	"wuelnerdotexe/vim-enfocado",
+	"catppuccin/nvim",
+  name = "catppuccin-mocha",
 	lazy = false,
 	priority = 1000,
 	config = function()
-		vim.cmd([[colorscheme enfocado]])
+		vim.cmd([[colorscheme catppuccin]])
 	end
 }
